74629b8126dd45fb5e5b87ff847d82702bde2ace
= <=>
2020-10-14 01:43:25 -0300

    segundo commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 $(document).foundation();
+
+
+let menu_open = false;
+
+
+
+
+$(document).ready( function () {
+
+
+
+	
+	if( window.screen.width < 640 ){
+		$("#menu_header").hide();	
+	}
+	
+
+	$("#button_menu_header").click( function(){
+		 
+		menu_open = !menu_open;
+
+		if(menu_open){
+			$("#menu_header").show();	
+		}else{
+			$("#menu_header").hide();	
+		} 
+		
+	})
+})
+
+
